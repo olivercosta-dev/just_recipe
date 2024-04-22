6898463200c1a241de402be6d6c20d86bacb561b
Oliver Costa <costaolika@gmail.com>
2024-04-22 20:47:43 +0200

    health_check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use just_recipe::routes::*;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(health_check));
+    
+    let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
+
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
+
