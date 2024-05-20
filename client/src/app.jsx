f9abe8fc4fa010ec3034f931a8af7cb67d56ffc3
Oliver Costa <costaolika@gmail.com>
2024-05-20 19:28:55 +0200

    initial commit again because I messed up

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,22 +1,11 @@
-import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import "./app.css";
+import logo from './logo.svg';
+import styles from './App.module.css';
 
-export default function App() {
+function App() {
+  // This is gonna be the Home Page for now.
   return (
-    <Router
-      root={props => (
-        <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <div class=''>Hello World!</div>
   );
 }
+
+export default App;
